ac5288d8906458bb5f3842e4dde5f0469dc10d5e
Daniel Ginting <daniel_yjg@yahoo.com>
2022-03-02 08:25:12 +0700

    initial commit

---
+++ RandomArrayItem.js
@@ -0,0 +1,30 @@
+const arr = ["A", "B", "C", "D", "E", "F", "G", "H"];
+let newArr = [];
+
+let random = 0;
+
+function genRandomArray (param){
+    return [param[Math.floor((Math.random() * (arr.length - 1)))],
+        param[Math.floor((Math.random() * (arr.length - 1)))],
+        param[Math.floor((Math.random() * (arr.length - 1)))]]
+}
+
+console.log(genRandomArray(arr))
+
+for (let i = 0; newArr.length !== 3; i++){
+    let array = genRandomArray(arr);
+    if  (
+            (
+                (arr.indexOf(array[1]) - 1) === arr.indexOf(array[2]) || 
+                (arr.indexOf(array[1]) + 1) === arr.indexOf(array[2])
+            ) &&
+            (
+                (arr.indexOf(array[1]) - 1) === arr.indexOf(array[0]) || 
+                (arr.indexOf(array[1]) + 1) === arr.indexOf(array[0])
+            )
+        ) 
+    {
+        newArr.push(array);
+    }
+}
+console.log(newArr)
