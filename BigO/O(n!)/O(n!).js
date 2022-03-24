d2eed6d9d0e2ad37629356fb1bd908fcf36485a9
Daniel Ginting <daniel_yjg@yahoo.com>
2022-03-24 11:10:58 +0700

    factorial time

---
+++ BigO/O(n!)/O(n!).js
@@ -0,0 +1,13 @@
+// O(n!) --> Factorial Time
+
+/* 
+Some of the transcript:
+We'll most likely won't encounter this notation. If we write any code that has O(n!), we're definitely doing something wrong. It is the most expensive one, it's the STEEPEST of them all. 
+It's called --> FACTORIAL TIME.(or Oh No!, just a joke).
+
+We won't do a demonstration on this because we just won't encounter it, just so that we know that it exists.
+
+With O(n!), (! is a mathematical notation) means that we're adding a nested loop for every input that we have.
+
+O(n!) -- add loop for every element.
+*/
